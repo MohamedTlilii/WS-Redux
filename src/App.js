f577f9c97668c7e8794919132f9528778335d935
NAUT5 <Mohamedtlili1995@hotmail.com>
2023-12-11 23:26:19 +0100

    almost-done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import "./App.css";
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { increase , decrease } from "./redux/actions";
 
 function App() {
-  let counter = useSelector((store)=>store)
-  console.log(counter)
+  const dispatch = useDispatch();
+  let counter = useSelector((store) => store);
+  console.log(counter);
   return (
     <div className="App">
       <h1>{counter}</h1>
       <div className="bnts">
-      <button className="font-tajawal">Increase</button>
-      <button>Decrease</button>
-    </div>
+        <button
+          className="font-tajawal"
+          onClick={() => {
+            dispatch(increase());
+          }}
+        >
+          Increase
+        </button>
+        <button onClick={() => {
+            dispatch(decrease());
+          }}>Decrease</button>
+      </div>
     </div>
   );
 }
--- src/Redux/Reducers/CounterReducer.js
+++ src/Redux/Reducers/CounterReducer.js
@@ -1,5 +1,11 @@
- const counterReducer = (counter = 10, action) => {
+ const counterReducer = (counter = 0, action) => {
+  if (action.type === 'INCREASE') {
+    return (counter = counter +1)
+  }else if (action.type === 'DECREASE' && counter > 0){
+    return (counter = counter -1)
+  }else {
   return counter;
+}
 };
 
 export default counterReducer;
